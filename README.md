e66f47aa8610036ca2d536fd7621f2d1c1415feb
Josh <69286716+thejoshyee@users.noreply.github.com>
2022-02-03 12:54:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Scrimba - Picsome
+
+## About
+Last project for Scrimba's Front-End Developer Career Path where we use React to mimic an ecommerce photo website. 
+
+## Learning
+- React
+	- Router
+	- Context
+	- State
+	- Props
+	- Use State
+	- Custom Hooks
+
+## Preview
+<img src="https://github.com/thejoshyee/pic-some/blob/main/picsome-preview1.png" width="50%" />
+<img src="https://github.com/thejoshyee/pic-some/blob/main/picsome-preview2.png" width="50%" />
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import React from "react"
+import {Routes, Route} from "react-router-dom"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+
+function App() {
+  return (
+    <div className="App">
+            <Header />
+            
+            <Routes>
+                <Route path="/" element={ <Photos /> } />
+                <Route path="/cart" element={<Cart />} />        
+            </Routes>
+    </div>
+  )
+}
+
+export default App;
